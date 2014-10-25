54b3bbedf5fa97c4ba1a965c440b73cbecafb560
Mohan Raj <mohanraj1311@gmail.com>
2014-10-25 03:07:22 -0400

    stringCompression

--- stringCompression.java
+++ stringCompression.java
@@ -6,6 +6,19 @@ There is no point in writing c1 when c is more compressed.
 Its because the compression itself shall fail.
 For example : compress : abc , its : a1b1c1, which is much longer
 than the origin string abc
+-----------------------
+Key idea: 
+Have a INT array initialised with 1 as content.
+This array shall have the no. of occurences of each of the alphabet.
+Since, the minimum count for any alphabet is 1.
+We initialise all the elements in the array as 1.
+
+Basic login lies in the if-else block.
+If block : if the consecutive alphabets are equal, there is nothing much to do.
+Just keep the count of the occurences.
+Else block : Once the consecutive alphabets are not equal, its time to
+insert the alphabet into the buffer and also insert the no. of occurences if the count exceeds 1.
+Continue by traversing, increasing the count of each 
 */
 
 import java.io.*;
@@ -19,7 +32,7 @@ class stringCompression{
 		//System.out.println("The length "+len);
 		StringBuffer compressedStringBuffer = new StringBuffer();
 
-		int countTheOccurences[] = new int[200];
+		int countTheOccurences[] = new int[len];
 
 		for (i = 0 ; i < len ; i++){
 			countTheOccurences[i] = 1;
@@ -30,7 +43,7 @@ class stringCompression{
 
 		while(i < len && j < len ){
 
-			if( i+1 < len && original.charAt(i)==original.charAt(i+1)){
+			if( i+1 < len && original.charAt(i)==original.charAt(i+1)){   // i + 1 < len , this condition is required to elimate the nullPointer exception error when we use charAt(i+1)
 
 				i++;
 				countTheOccurences[j]++;
@@ -63,3 +76,16 @@ class stringCompression{
 
 	}
 }
+
+
+
+/*
+Sample output :
+
+mohan@mohans ~/javaPgms $ java stringCompression
+Enter the original String
+aaabbbbbccccc
+The compressed String is a3b5c5
+mohan@mohans ~/javaPgms $ 
+
+*/
